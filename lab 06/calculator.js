c14a7c55395b3998f3675f77877bc7e470a2b866
Nalexie <96800692+Nalexie@users.noreply.github.com>
2022-02-22 16:09:00 -0800

    Add files via upload

---
+++ lab 06/calculator.js
@@ -0,0 +1,43 @@
+function addition() {
+    a = parseInt(jQuery("#a").val());
+    b = parseInt(jQuery("#b").val());
+    jQuery('#p1').html(a+"+"+b+"="+(a+b))
+    old = jQuery("#history").html();
+    jQuery("#history").html(old +"<br>"+ "<span class='red'>"+a+"+"+b+"="+(a+b)+"<span>");
+
+}
+
+function subtraction() {
+    a = parseInt(jQuery("#a").val());
+    b = parseInt(jQuery("#b").val());
+    jQuery('#p1').html(a+"-"+b+"="+(a-b))
+    old = jQuery("#history").html();
+    jQuery("#history").html(old +"<br>"+ "<span class='blue'>"+a+"-"+b+"="+(a-b)+"<span>");
+}
+
+function multiplication() {
+    a = parseInt(jQuery("#a").val());
+    b = parseInt(jQuery("#b").val());
+    jQuery('#p1').html(a+"*"+b+"="+(a * b))
+    old = jQuery("#history").html();
+    jQuery("#history").html(old +"<br>"+ "<span class='green'>"+a+"*"+b+"="+(a * b)+"<span>");
+}
+
+function division() {
+    a = parseInt(jQuery("#a").val());
+    b = parseInt(jQuery("#b").val());
+    jQuery('#p1').html(a+"/"+b+"="+(a/b))
+    old = jQuery("#history").html();
+    jQuery("#history").html(old +"<br>"+ "<span class='pantone'>"+a+"/"+b+"="+(a/b)+"<span>");
+}
+
+
+function setup() {
+    jQuery("#addition").click(addition);
+    jQuery("#subtraction").click(subtraction);
+    jQuery("#multiplication").click(multiplication);
+    jQuery("#division").click(division);
+}
+jQuery(document).ready(setup);
+
+console.log("hi")
